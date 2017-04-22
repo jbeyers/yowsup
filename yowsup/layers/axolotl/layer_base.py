63ff8c339143620df0e26ac9e07aea7f3528bf0d
Johan Beyers <johan@juizi.com>
2017-04-22 16:39:00 +0200

    Broadcast with text message, pre-image for reference

--- yowsup/layers/axolotl/layer_base.py
+++ yowsup/layers/axolotl/layer_base.py
@@ -78,7 +78,7 @@ class AxolotlBaseLayer(YowProtocolLayer):
 
         def onError(errorNode, getKeysEntity):
             if errorClbk:
-                errorClbk(errorNode, getKeysEntity)
+                errorClbk()
 
         entity = GetKeysIqProtocolEntity(jids)
         self._sendIq(entity, onSuccess, onError=onError)
